ca7b17394aa9a01efe3dc55c70ca4454b150781e
bhaweshrai1976 <bhaweshrai1976@gmail.com>
2026-03-02 10:19:44 +0530

    Initial commit

---
+++ UC1.java
@@ -0,0 +1,22 @@
+//blic class PalindromeCheckerApp{
+//blic static main void (string[] args){
+//splaymessage();
+//
+//
+//splaymessage(){
+//stem.out.println("Welcomr to the Palindrome Checker management system");
+//stem.out.println("Version : 1");
+//stem.out.println("System initialized successfully");
+//
+public class UC1 {
+
+    public static void main(String[] args) {
+        displayMessage();
+    }
+
+    public static void displayMessage() {
+        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Version : 1");
+        System.out.println("System initialized successfully");
+    }
+}
