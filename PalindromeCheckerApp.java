c2fb3917b1b62624f93bd75103b80d57f9bdb09a
bhaweshrai1976 <bhaweshrai1976@gmail.com>
2026-03-02 10:38:55 +0530

    this a develop branch

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -1,13 +1,9 @@
 
 public class PalindromeCheckerApp {
-    public static void displayMessage() {
-        System.out.println("Welcome to the Palindrome Checker Management System");
-        System.out.println("Version : 1");
-        System.out.println("System initialized successfully");
-    }
+
 
     public static void main(String[] args) {
-        displayMessage();
+
     }
 
 
