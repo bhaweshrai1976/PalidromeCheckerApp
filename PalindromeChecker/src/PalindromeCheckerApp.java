046fc9616115498965c66bc6f48f8c01e36c347d
bhaweshrai1976 <bhaweshrai1976@gmail.com>
2026-03-02 10:41:07 +0530

    UC1: welecome message

--- PalindromeChecker/src/PalindromeCheckerApp.java
+++ PalindromeChecker/src/PalindromeCheckerApp.java
@@ -1,3 +1,14 @@
-public class PalindromeCheckerApp{
+package PalindromeChecker.src;
 
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+        displayMessage();
+    }
+
+    public static void displayMessage() {
+        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Version : 1");
+        System.out.println("System initialized successfully");
+    }
 }
