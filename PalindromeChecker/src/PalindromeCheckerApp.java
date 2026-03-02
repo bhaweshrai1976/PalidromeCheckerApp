b41d68f14e8371e66b51fec7ab36b71c4f324a08
bhaweshrai1976 <bhaweshrai1976@gmail.com>
2026-03-02 11:18:42 +0530

    UC3

--- PalindromeChecker/src/PalindromeCheckerApp.java
+++ PalindromeChecker/src/PalindromeCheckerApp.java
@@ -3,17 +3,16 @@ import java.util.*;
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
         String input = "madam";
+        String reversed = "";
         boolean isPalindrome = true;
 
         System.out.println("Input: " + input);
 
-        for (int i = 0; i < input.length() / 2; i++) {
-            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
-                isPalindrome = false;
-                break;
-            }
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed = reversed + input.charAt(i);
         }
-
-      System.out.println("Is Palindrome ? :" + isPalindrome);
+        if (input.equals(reversed)) {
+            System.out.println("Is Palindrome ?:" + isPalindrome);
+        }
     }
 }
